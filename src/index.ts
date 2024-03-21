c01ed48d449eaa7d0e88479b44863860b12cc54e
nick-leslie <nick.leslie303@gmail.com>
2024-03-21 12:09:22 -0400

    created repo

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "../.prisma/client";
+const prisma = new PrismaClient()
+
+// prisma.example.create({
+//     data:{
+//         text:"test"
+//     }
+// })
+console.log("hello world")
