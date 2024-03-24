2ff5651554d8357a68a14477b8dfd993134a2704
KimAllise32 <114198322+KimAllise32@users.noreply.github.com>
2024-03-23 21:00:54 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,25 @@ import { PrismaClient } from "../.prisma/client";
 import readCSVFile from "./Readcsv";
 const prisma = new PrismaClient()
 
-// prisma.example.create({
-//     data:{
-//         text:"test"
-//     }
-// })
+
 const edges = readCSVFile('L1Edges.csv');
 const nodes = readCSVFile('L1Nodes.csv');
+
+console.log();
+console.log("Edges CSV File")
+for (let i = 0; i < edges.length; i++) {
+    for (let j = 0; j < edges[i].length; j++) {
+        process.stdout.write(edges[i][j] + "\t");
+    }
+    console.log();
+}
+
+console.log();
+console.log("\nNodes CSV File")
+
+for (let i = 0; i < nodes.length; i++) {
+    for (let j = 0; j < nodes[i].length; j++) {
+        process.stdout.write(nodes[i][j] + "\t");
+    }
+    console.log();
+}
