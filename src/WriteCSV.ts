77d05cfefbf16d45a54cc326ab1eff15c93ee83d
Matt <mattc@mill5.com>
2024-03-27 11:26:20 -0400

    Fixed WriteCSV function a bit and added functionality code in index

--- src/WriteCSV.ts
+++ src/WriteCSV.ts
@@ -1,17 +1,17 @@
-import { createObjectCsvWriter as createCsvWriter } from 'csv-writer';
+import { createObjectCsvWriter as createCsvWriter } from "csv-writer";
 
 interface CsvData {
-    headers: { id: string, title: string }[];
-    data: any[];
+  headers: { id: string; title: string }[];
+  data: any[];
 }
 
-async function createCsvFile(data: CsvData, path: string) {
-    const csvWriter = createCsvWriter({
-        path: path,
-        header: data.headers,
-    });
+function createCsvFile(data: CsvData, path: string) {
+  const csvWriter = createCsvWriter({
+    path: path,
+    header: data.headers,
+  });
 
-    await csvWriter.writeRecords(data.data);
-    console.log(`CSV file written to ${path}`);
+  csvWriter.writeRecords(data.data);
+  console.log(`CSV file written to ${path}`);
 }
-
+export default createCsvFile;
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,61 @@
 import { PrismaClient } from "../.prisma/client";
 import readCSVFile from "./Readcsv";
-const prisma = new PrismaClient()
+import createCSVFile from "./WriteCSV";
 
+const prisma = new PrismaClient();
 
-const edges = readCSVFile('L1Edges.csv');
-const nodes = readCSVFile('L1Nodes.csv');
+const edges = readCSVFile("L1Edges.csv");
+const nodes = readCSVFile("L1Nodes.csv");
 
 console.log();
-console.log("Edges CSV File")
+console.log("Edges CSV File");
 for (let i = 0; i < edges.length; i++) {
-    for (let j = 0; j < edges[i].length; j++) {
-        process.stdout.write(edges[i][j] + "\t");
-    }
-    console.log();
+  for (let j = 0; j < edges[i].length; j++) {
+    process.stdout.write(edges[i][j] + "\t");
+  }
+  console.log();
 }
 
 console.log();
-console.log("\nNodes CSV File")
+console.log("\nNodes CSV File");
 
 for (let i = 0; i < nodes.length; i++) {
-    for (let j = 0; j < nodes[i].length; j++) {
-        process.stdout.write(nodes[i][j] + "\t");
-    }
-    console.log();
+  for (let j = 0; j < nodes[i].length; j++) {
+    process.stdout.write(nodes[i][j] + "\t");
+  }
+  console.log();
 }
+const edgeData = {
+  headers: [
+    { id: "startNodeID", title: "startNodeID" },
+    { id: "endNodeID", title: "endNodeID" },
+  ],
+  data: [{ id: 1, startNodeID: "CCONF002L1", endNodeID: "WELEV00HL1" }],
+};
+createCSVFile(edgeData, "L1EdgesTest.csv");
+const nodeData = {
+  headers: [
+    { id: "nodeID", title: "nodeID" },
+    { id: "xcoord", title: "xcoord" },
+    { id: "ycoord", title: "ycoord" },
+    { id: "floor", title: "floor" },
+    { id: "building", title: "building" },
+    { id: "nodeType", title: "nodeType" },
+    { id: "longName", title: "longName" },
+    { id: "shortName", title: "shortName" },
+  ],
+  data: [
+    {
+      id: 1,
+      nodeID: "CCONF001L1",
+      xcoord: 2255,
+      ycoord: 849,
+      floor: "L1",
+      building: "45 Francis",
+      nodeType: "CONF",
+      longName: "Anesthesia Conf Floor L1",
+      shortName: "Conf C001L1",
+    },
+  ],
+};
+createCSVFile(nodeData, "L1NodesTest.csv");
